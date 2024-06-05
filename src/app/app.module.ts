406121efb1ca234d35a65f4b66d9762cb1fe3d4c
mahanasoumyaranjan <116441447+mahanasoumyaranjan@users.noreply.github.com>
2024-06-05 19:37:07 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,3 @@
-// import { NgModule } from '@angular/core';
-// import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
-// import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
-// import { EmployeeListComponent } from './employee-list/employee-list.component';
-// import { HttpClientModule } from '@angular/common/http';
-// import { FormsModule } from '@angular/forms';
-// import { EmployeeAddComponent } from './employee-add/employee-add.component';
-// import { EmployeeUpdateComponent } from './employee-update/employee-update.component';
-
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     EmployeeListComponent,
-//     EmployeeAddComponent,
-//     EmployeeUpdateComponent
-//   ],
-//   imports: [
-//     BrowserModule,
-//     AppRoutingModule, HttpClientModule, FormsModule
-//   ],
-//   providers: [
-//     provideClientHydration()
-//   ],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
-
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
